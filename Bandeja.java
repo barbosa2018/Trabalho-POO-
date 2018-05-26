ecac958c4907f20ac50ac1bdc83752207da75691
barbosa2018 <39637552+barbosa2018@users.noreply.github.com>
2018-05-25 23:17:30 -0300

    Add files via upload

---
+++ Bandeja.java
@@ -0,0 +1,29 @@
+
+
+/** *
+ * @author IgorX
+ */
+
+public class Bandeja {
+    private double valorDeFace;
+    private int quantidadedeCedula;
+    
+    public Bandeja(double valorDeFace, int quantidadedeCedula){
+        this.valorDeFace = valorDeFace;
+        this.quantidadedeCedula = quantidadedeCedula;
+    
+    }
+    public double getvalorDeFace(){
+        return this.valorDeFace;
+    }
+    public void setvalordeFace(double valorDeFace){
+        this.valorDeFace = valorDeFace;
+    }
+    public int getquantidadedeCedula(){
+        return this.quantidadedeCedula;
+    }
+    public void setquantidadedeCedula(int quantidadedeCedula){
+        this.quantidadedeCedula = quantidadedeCedula;
+    }
+    
+}
